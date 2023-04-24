7c52fa473830a2e2b61249c975d79d45c184be5a
burnout131 <marcrg07@gmail.com>
2023-04-24 22:30:20 +0200

    Removed unnecessary imports at Cliente.java and Dentista.java

--- EX02/src/main/java/com/mrojo/ex02/dto/Dentista.java
+++ EX02/src/main/java/com/mrojo/ex02/dto/Dentista.java
@@ -11,7 +11,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
